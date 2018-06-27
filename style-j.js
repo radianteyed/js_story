46bb589648ddf920947b25ba567a9bf54515a7e5
radianteyed <radiant.eyed@gmail.com>
2018-06-27 17:27:22 +0200

    Initial commit

---
+++ style-j.js
@@ -0,0 +1,9 @@
+var animal = prompt ("Напиши одно любое животное");
+
+var actionAnim  = prompt ('Напиши одно любое дейсвие');
+var color = prompt ("Напиши любимый цвет");
+
+var answer = "Жил-был " + animal + " и как-то однажды захотел он " + actionAnim + " хомячков. Буба на него обиделась и " + animal + " стал " + color + ". Конец.";
+
+
+document.write (answer);
